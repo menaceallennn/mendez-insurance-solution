84eef1e58be2850a4236cd4b40ac8b2b78d911af
menaceallennn <allenblack10@hotmail.com>
2026-01-01 14:18:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Mendez Insurance site loaded");
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your information was sent.");
+});
